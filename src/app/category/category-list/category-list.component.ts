e796943c68f34e714df4c5bd2f2c62b4b6a64c55
schobinho <markus.schober@lernen.swiss.siw>
2023-11-21 21:02:31 +0100

    Neue funktionen für Sicherung und Suche

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CategoryModalComponent } from '../category-modal/category-modal.component';
 import {InfiniteScrollCustomEvent, ModalController, RefresherCustomEvent} from '@ionic/angular';
-import {Category, CategoryCriteria} from '../../shared/domain';
+import {Category, CategoryCriteria, SortOption} from '../../shared/domain';
 import {CategoryService} from "../category.service";
 import {ToastService} from "../../shared/service/toast.service";
-import {finalize} from "rxjs";
+import {finalize, Subscription} from "rxjs";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-category-list',
@@ -33,10 +34,20 @@ export class CategoryListComponent {
     const { role } = await modal.onWillDismiss();
     if (role === 'refresh') this.reloadCategories();
   }
+  readonly searchForm: FormGroup;
+  readonly sortOptions: SortOption[] = [
+    { label: 'Created at (newest first)', value: 'createdAt,desc' },
+    { label: 'Created at (oldest first)', value: 'createdAt,asc' },
+    { label: 'Name (A-Z)', value: 'name,asc' },
+    { label: 'Name (Z-A)', value: 'name,desc' },
+  ];
+  private readonly searchFormSubscription: Subscription;
+
   constructor(
     private readonly modalCtrl: ModalController,
     private readonly categoryService: CategoryService,
-    private readonly toastService: ToastService
+    private readonly toastService: ToastService,
+    private readonly formBuilder: FormBuilder
   ) {}
 
   private loadCategories(next: () => void = () => {}): void {
