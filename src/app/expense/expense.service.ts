79492d848148217512532f4d196c700d6ddaffdb
schobinho <markus.schober@lernen.swiss.siw>
2023-11-24 15:12:47 +0100

    Aufgabe 3 - Neues Expense.service.ts

---
+++ src/app/expense/expense.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import {
+  PagingCriteria,
+  Page,
+  Expense,
+  ExpenseCriteria,
+  ExpenseUpsertDto,
+  SortOption,
+  CategoryCriteria, Category
+} from "../shared/domain";
+
+@Injectable({ providedIn: 'root' })
+export class ExpenseService {
+  private readonly apiUrl = `${environment.backendUrl}/expenses`;
+  private readonly apiV2Url = `${environment.backendUrl}/v2/expenses`;
+
+  constructor(private readonly httpClient: HttpClient) {}
+
+  // Suchen
+
+  getExpenses = (pagingCriteria: ExpenseCriteria): Observable<Page<Expense>> =>
+    this.httpClient.get<Page<Expense>>(this.apiUrl, { params: new HttpParams({ fromObject: { ...pagingCriteria } }) });
+
+
+  // Erstellen & Bearbeiten
+
+  upsertExpense = (expense: Expense): Observable<void> => this.httpClient.put<void>(this.apiUrl, expense);
+
+  // Löschen
+
+  deleteExpense = (id: string): Observable<void> => this.httpClient.delete<void>(`${this.apiUrl}/${id}`);
+}
